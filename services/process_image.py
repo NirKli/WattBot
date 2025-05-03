0a5dce93be7ec19a442a2c28ac28067ebac68e8d
Nir <klinger.nir@gmail.com>
2025-05-03 19:19:54 +0300

    Add new image data files and update class names for model training

--- services/process_image.py
+++ services/process_image.py
@@ -1,6 +1,8 @@
 import os
 import re
 import shutil
+import torch
+import cv2
 
 from PIL import Image
 from paddleocr import PaddleOCR
@@ -17,29 +19,21 @@ class ProcessImage:
         with open(temp_file_path, "wb") as temp_file:
             shutil.copyfileobj(file.file, temp_file)
 
-        results = self.model(temp_file_path, save=True, project="process_imgs")  # Predict on an image
-        results[0].show()  # Display results
+        results = self.model(temp_file_path, save=True, project="process_imgs", rect=True)
+        results[0].show()
 
-        image = Image.open(f"{temp_file_path}")  # Display results
-        # Image.open(f"{temp_file_path}").show()  # Display results
-        boxes = results[0].boxes.xyxy[0]  # get boxes from the first image
-        image.crop(boxes[0], boxes[1], boxes[2], boxes[3])
+        detections = []
+        for box in results[0].boxes:
+            cls = int(box.cls.item())
+            conf = float(box.conf.item())
+            label = self.model.names[cls]
+            x_center = box.xywh[0][0].item()
+            detections.append((x_center, label, conf))
 
-        paddleocr = PaddleOCR(use_angle_cls=True, det_db_box_thresh=0.1, drop_score=0.7,
-                              lang='en')  # need to run only once to download and load model into memory
-        results = paddleocr.ocr(temp_file_path, cls=True)
-        for result in results:
-            #   print(f"Result: {result}")
-            for line in result:
-                #   print(f"Line: {line}")
-                for word_info in line:
-                    #     print(f"Word Info: {word_info}")
-                    for word in word_info:
-                        #    print(f"Word: {word}")
-                        if isinstance(word, str):
-                            if re.fullmatch("\d+(\.\d+)?", word):
-                                print(f"Word is: {word}")
-                                os.remove(temp_file_path)
-                                return word
+        detections.sort(key=lambda x: x[0])
 
-        os.remove(temp_file_path)
+        output = ''.join([lbl for _, lbl, _ in detections])
+        with_conf = ' '.join([f"{lbl}:{conf:.2f}" for _, lbl, conf in detections])
+
+        print("Predicted Number:", output)
+        print("Digits with Confidence:", with_conf)
