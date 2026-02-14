e6acee33ce70dad9751246a68e349f524a18df5c
Nir <klinger.nir@gmail.com>
2026-02-14 18:16:29 +0200

    upgrade model to yolo26s-obb

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 # base model
 if __name__ == "__main__":
-    model = YOLO("yolo11s-obb.pt")
+    model = YOLO("yolo26s-obb.pt")
     model.train(data='digits-detection/data.yaml', epochs=200, imgsz=1280, batch=12, name='lcd_detector', device=0,
                 rect=False, degrees=15,
                 translate=0.1,
